769f6763c411f2e4182bd7803ecd71879c1c2bae
Dawid Bryja <dawbryja@gmail.com>
2022-10-20 21:01:32 +0200

    add swiping support

--- 9.Scroller/index.js
+++ 9.Scroller/index.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   const scroller = new Scroller(".main");
+  const swiper = new Swiper();
 
   document.addEventListener("wheel", function (event) {
     scroller.listenScroll(event);
   });
 
-  document.addEventListener("swipeUp", () => console.log("up"));
-  document.addEventListener("swipeDown", () => console.log("down"));
+  document.addEventListener("swipeUp", () => scroller.scroll(1));
+  document.addEventListener("swipeDown", () => scroller.scroll(-1));
 });
--- 9.Scroller/swiper.js
+++ 9.Scroller/swiper.js
@@ -3,13 +3,6 @@ class Swiper {
     this.initialY = null;
     this.initialX = null;
 
-    document.addEventListener("touchstart", (event) => {
-      this.startTouch(event);
-    });
-    document.addEventListener("touchmove", (event) => {
-      this.moveTouch;
-    });
-
     this.events = {
       swipeUp: new Event("swipeUp"),
       swipeDown: new Event("swipeDown"),
@@ -17,35 +10,41 @@ class Swiper {
       swipeLeft: new Event("swipeLeft"),
     };
 
-    startTouch((e) => {
-      e.preventDefault();
-      this.initialX = e.touches[0].clientX;
-      this.initialY = e.touches[0].clientY;
-    });
-
-    moveTouch((e) => {
-      e.preventDefault();
-      if (!this.initialX || !this.initialY) return;
-
-      this.currentX = e.touches[0].clientX;
-      this.currentY = e.touches[0].clientY;
-
-      const diffX = this.initialX - this.currentX;
-      const diffY = this.initialY - this.currentY;
-
-      if (Math.abs(diffX) > Math.abs(diffY)) {
-        if (diffX > 0) {
-          document.dispatchEvent(this.events.swipeLeft);
-        } else {
-          document.dispatchEvent(this.events.swipeRight);
-        }
-      } else {
-        if (diffY > 0) {
-          document.dispatchEvent(this.events.swipeUp);
-        } else {
-          document.dispatchEvent(this.events.swipeDown);
-        }
-      }
-    });
+    document.addEventListener("touchstart", this.startTouch);
+    document.addEventListener("touchmove", this.moveTouch);
   }
+
+  startTouch = (e) => {
+    e.preventDefault();
+    this.initialX = e.touches[0].clientX;
+    this.initialY = e.touches[0].clientY;
+  };
+
+  moveTouch = (e) => {
+    e.preventDefault();
+    if (!this.initialX || !this.initialY) return;
+
+    this.currentX = e.touches[0].clientX;
+    this.currentY = e.touches[0].clientY;
+
+    const diffX = this.initialX - this.currentX;
+    const diffY = this.initialY - this.currentY;
+
+    if (Math.abs(diffX) > Math.abs(diffY)) {
+      if (diffX > 0) {
+        document.dispatchEvent(this.events.swipeLeft);
+      } else {
+        document.dispatchEvent(this.events.swipeRight);
+      }
+    } else {
+      if (diffY > 0) {
+        document.dispatchEvent(this.events.swipeUp);
+      } else {
+        document.dispatchEvent(this.events.swipeDown);
+      }
+    }
+
+    this.initialY = null;
+    this.initialX = null;
+  };
 }
